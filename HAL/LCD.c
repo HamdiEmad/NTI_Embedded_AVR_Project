3d2812f85db430817f118efd74477e931ce5f20a
shahd hussein <shahdhussein7777@gmail.com>
2026-09-23 21:36:01 +0300

    Add files via upload

---
+++ HAL/LCD.c
@@ -0,0 +1,170 @@
+/*
+ * LCD.c
+ *
+ *  Created on: Sep 21, 2026
+ *      Author: win 10
+ */
+#include "../../LIB/BitMath.h"
+#include "../../LIB/STD_types.h"
+
+#include "../../MCAL/MDIO/DIO.h"
+
+#include "LCD.h"
+#include <util/delay.h>
+void HLCD_voidInit (void)
+{
+#if LCD_ == 8
+	_delay_ms(40) ;
+	MDIO_voidInitPort(LCD_DATA_PORT , 0xFF) ;
+	MDIO_voidInitPin(LCD_RS_PORT , LCD_RS_PIN , DIO_OUTPUT) ;
+	MDIO_voidInitPin(LCD_RW_PORT , LCD_RW_PIN , DIO_OUTPUT) ;
+	MDIO_voidInitPin(LCD_E_PORT , LCD_E_PIN , DIO_OUTPUT) ;
+	HLCD_voidSendCmd(0x38) ;
+	_delay_ms(1) ;
+	HLCD_voidSendCmd(0x0E) ;
+	_delay_ms(1) ;
+	HLCD_voidSendCmd(0x01) ;
+	_delay_ms(4) ;
+	HLCD_voidSendCmd(0x06) ;
+	_delay_ms(1) ;
+#elif LCD_MODE == 4
+	_delay_ms(40) ;
+	MDIO_voidInitPin(LCD_DATA_PORT , DIO_PIN4 , DIO_OUTPUT) ;
+	MDIO_voidInitPin(LCD_DATA_PORT , DIO_PIN5 , DIO_OUTPUT) ;
+	MDIO_voidInitPin(LCD_DATA_PORT , DIO_PIN6 , DIO_OUTPUT) ;
+	MDIO_voidInitPin(LCD_DATA_PORT , DIO_PIN7 , DIO_OUTPUT) ;
+	MDIO_voidInitPin(LCD_RS_PORT , LCD_RS_PIN , DIO_OUTPUT) ;
+	MDIO_voidInitPin(LCD_RW_PORT , LCD_RW_PIN , DIO_OUTPUT) ;
+	MDIO_voidInitPin(LCD_E_PORT , LCD_E_PIN , DIO_OUTPUT) ;
+	HLCD_voidSendCmd(0x02) ;
+	_delay_ms(1) ;
+	HLCD_voidSendCmd(0x28) ;
+	_delay_ms(1) ;
+	HLCD_voidSendCmd(0x0E) ;
+	_delay_ms(1) ;
+	HLCD_voidSendCmd(0x01) ;
+	_delay_ms(4) ;
+	HLCD_voidSendCmd(0x06) ;
+	_delay_ms(1) ;
+
+
+#else
+#error "invalid LCD_MODE"
+#endif
+}
+void HLCD_voidSendData (u8 A_u8Data)
+{
+#if LCD_MODE == 8
+	MDIO_voidSetPinValue(LCD_RS_PORT , LCD_RS_PIN , DIO_HIGH) ;
+	MDIO_voidSetPinValue(LCD_RW_PORT , LCD_RW_PIN , DIO_LOW) ;
+	MDIO_voidSetPortValue(LCD_DATA_PORT , A_u8Data) ;
+	MDIO_voidSetPinValue(LCD_E_PORT , LCD_E_PIN , DIO_HIGH) ;
+	_delay_ms (1) ;
+	MDIO_voidSetPinValue(LCD_E_PORT , LCD_E_PIN , DIO_LOW) ;
+	_delay_ms (1) ;
+#elif LCD_MODE == 4
+	MDIO_voidSetPinValue(LCD_RS_PORT , LCD_RS_PIN , DIO_HIGH) ;
+	MDIO_voidSetPinValue(LCD_RW_PORT , LCD_RW_PIN , DIO_LOW) ;
+	MDIO_voidSetPinValue(LCD_DATA_PORT , DIO_PIN4 , READ_BIT(A_u8Data , 4)) ;
+	MDIO_voidSetPinValue(LCD_DATA_PORT , DIO_PIN5 , READ_BIT(A_u8Data , 5)) ;
+	MDIO_voidSetPinValue(LCD_DATA_PORT , DIO_PIN6 , READ_BIT(A_u8Data , 6)) ;
+	MDIO_voidSetPinValue(LCD_DATA_PORT , DIO_PIN7 , READ_BIT(A_u8Data , 7)) ;
+	MDIO_voidSetPinValue(LCD_E_PORT , LCD_E_PIN , DIO_HIGH) ;
+	_delay_ms (1) ;
+	MDIO_voidSetPinValue(LCD_E_PORT , LCD_E_PIN , DIO_LOW) ;
+	_delay_ms (1) ;
+
+	MDIO_voidSetPinValue(LCD_RS_PORT , LCD_RS_PIN , DIO_HIGH) ;
+	MDIO_voidSetPinValue(LCD_RW_PORT , LCD_RW_PIN , DIO_LOW) ;
+	MDIO_voidSetPinValue(LCD_DATA_PORT , DIO_PIN4 , READ_BIT(A_u8Data , 0)) ;
+	MDIO_voidSetPinValue(LCD_DATA_PORT , DIO_PIN5 , READ_BIT(A_u8Data , 1)) ;
+	MDIO_voidSetPinValue(LCD_DATA_PORT , DIO_PIN6 , READ_BIT(A_u8Data , 2)) ;
+	MDIO_voidSetPinValue(LCD_DATA_PORT , DIO_PIN7 , READ_BIT(A_u8Data , 3)) ;
+	MDIO_voidSetPinValue(LCD_E_PORT , LCD_E_PIN , DIO_HIGH) ;
+	_delay_ms (1) ;
+	MDIO_voidSetPinValue(LCD_E_PORT , LCD_E_PIN , DIO_LOW) ;
+	_delay_ms (1) ;
+#endif
+}
+void HLCD_voidSendCmd (u8 A_u8Cmd)
+{
+#if LCD_MODE == 8
+	MDIO_voidSetPinValue(LCD_RS_PORT , LCD_RS_PIN , DIO_LOW) ;
+	MDIO_voidSetPinValue(LCD_RW_PORT , LCD_RW_PIN , DIO_LOW) ;
+	MDIO_voidSetPortValue(LCD_DATA_PORT , A_u8Cmd) ;
+	MDIO_voidSetPinValue(LCD_E_PORT , LCD_E_PIN , DIO_HIGH) ;
+	_delay_ms (1) ;
+	MDIO_voidSetPinValue(LCD_E_PORT , LCD_E_PIN , DIO_LOW) ;
+	_delay_ms (1) ;
+#elif LCD_MODE == 4
+	MDIO_voidSetPinValue(LCD_RS_PORT , LCD_RS_PIN , DIO_LOW) ;
+	MDIO_voidSetPinValue(LCD_RW_PORT , LCD_RW_PIN , DIO_LOW) ;
+	MDIO_voidSetPinValue(LCD_DATA_PORT , DIO_PIN4 , READ_BIT(A_u8Cmd , 4)) ;
+	MDIO_voidSetPinValue(LCD_DATA_PORT , DIO_PIN5 , READ_BIT(A_u8Cmd , 5)) ;
+	MDIO_voidSetPinValue(LCD_DATA_PORT , DIO_PIN6 , READ_BIT(A_u8Cmd , 6)) ;
+	MDIO_voidSetPinValue(LCD_DATA_PORT , DIO_PIN7 , READ_BIT(A_u8Cmd , 7)) ;
+	MDIO_voidSetPinValue(LCD_E_PORT , LCD_E_PIN , DIO_HIGH) ;
+	_delay_ms (1) ;
+	MDIO_voidSetPinValue(LCD_E_PORT , LCD_E_PIN , DIO_LOW) ;
+	_delay_ms (1) ;
+
+	MDIO_voidSetPinValue(LCD_RS_PORT , LCD_RS_PIN , DIO_LOW) ;
+	MDIO_voidSetPinValue(LCD_RW_PORT , LCD_RW_PIN , DIO_LOW) ;
+	MDIO_voidSetPinValue(LCD_DATA_PORT , DIO_PIN4 , READ_BIT(A_u8Cmd , 0)) ;
+	MDIO_voidSetPinValue(LCD_DATA_PORT , DIO_PIN5 , READ_BIT(A_u8Cmd , 1)) ;
+	MDIO_voidSetPinValue(LCD_DATA_PORT , DIO_PIN6 , READ_BIT(A_u8Cmd , 2)) ;
+	MDIO_voidSetPinValue(LCD_DATA_PORT , DIO_PIN7 , READ_BIT(A_u8Cmd , 3)) ;
+	MDIO_voidSetPinValue(LCD_E_PORT , LCD_E_PIN , DIO_HIGH) ;
+	_delay_ms (1) ;
+	MDIO_voidSetPinValue(LCD_E_PORT , LCD_E_PIN , DIO_LOW) ;
+	_delay_ms (1) ;
+#endif
+}
+void HLCD_voidSendStr (u8* AP_u8str)
+{
+	u8 L_u8Iterator = 0 ;
+	while (AP_u8str [L_u8Iterator] != '\0')
+	{
+		HLCD_voidSendData (AP_u8str [L_u8Iterator]) ;
+		L_u8Iterator ++ ;
+	}
+}
+void HLCD_voidClrDisplay (void)
+{
+	HLCD_voidSendCmd(0x01) ;
+	_delay_ms(4) ;
+}
+void HLCD_voidShiftLeft (void)
+{
+	HLCD_voidSendCmd(0x18) ;
+	_delay_ms(1) ;
+}
+void HLCD_voidShiftRight (void)
+{
+	HLCD_voidSendCmd(0x1C) ;
+	_delay_ms(1) ;
+}
+void HLCD_voidSetCursor (u8 A_u8Row , u8 A_u8Col)
+{
+	u8 L_u8AC ;
+	if (A_u8Row == LCD_ROW0)
+	{
+		L_u8AC = A_u8Col ;
+	}
+	else if (A_u8Row == LCD_ROW1)
+	{
+		L_u8AC = 0x40 + A_u8Col ;
+	}
+	HLCD_voidSendCmd(L_u8AC + 0x80) ;
+}
+void HLCD_voidSendCustomChar (u8 arr [] , u8 A_u8BlockNumber , u8 A_u8Row , u8 A_u8Col)
+{
+	u8 L_u8Iterator ;
+	HLCD_voidSendCmd(A_u8BlockNumber*8 + 0x40) ;
+	for (L_u8Iterator = 0 ; L_u8Iterator < 8 ; L_u8Iterator ++)
+	{
+		HLCD_voidSendData(arr [L_u8Iterator]) ;
+	}
+	HLCD_voidSetCursor(A_u8Row , A_u8Col) ;
+	HLCD_voidSendData(A_u8BlockNumber) ;
+}
---
+++ HAL/LCD.h
@@ -0,0 +1,66 @@
+/*
+ * LCD.h
+ *
+ *  Created on: Sep 21, 2026
+ *      Author: win 10
+ */
+
+#ifndef HAL_HLCD_LCD_H_
+#define HAL_HLCD_LCD_H_
+
+#define LCD_DATA_PORT  DIO_PORTA
+
+#define LCD_RS_PORT    DIO_PORTB
+#define LCD_RS_PIN     DIO_PIN0
+
+#define LCD_RW_PORT    DIO_PORTB
+#define LCD_RW_PIN     DIO_PIN1
+
+#define LCD_E_PORT    DIO_PORTB
+#define LCD_E_PIN     DIO_PIN2
+
+#define LCD_MODE      4
+
+#define LCD_ROW0      0
+#define LCD_ROW1      1
+
+
+#define LCD_COL0     0
+#define LCD_COL1     1
+#define LCD_COL2     2
+#define LCD_COL3     3
+#define LCD_COL4     4
+#define LCD_COL5     5
+#define LCD_COL6     6
+#define LCD_COL7     7
+#define LCD_COL8     8
+#define LCD_COL9     9
+#define LCD_COL10    10
+#define LCD_COL11    11
+#define LCD_COL12    12
+#define LCD_COL13    13
+#define LCD_COL14    14
+#define LCD_COL15    15
+
+#define LCD_BLOCK0   0
+#define LCD_BLOCK1   1
+#define LCD_BLOCK2   2
+#define LCD_BLOCK3   3
+#define LCD_BLOCK4   4
+#define LCD_BLOCK5   5
+#define LCD_BLOCK6   6
+#define LCD_BLOCK7   7
+
+
+
+
+void HLCD_voidInit (void) ;
+void HLCD_voidSendData (u8 A_u8Data) ;
+void HLCD_voidSendCmd (u8 A_u8Cmd) ;
+void HLCD_voidSendStr (u8* AP_u8str) ;
+void HLCD_voidClrDisplay (void) ;
+void HLCD_voidShiftLeft (void) ;
+void HLCD_voidShiftRight (void) ;
+void HLCD_voidSetCursor (u8 A_u8Row , u8 A_u8Col) ;
+void HLCD_voidSendCustomChar (u8 arr [] , u8 A_u8BlockNumber , u8 A_u8Row , u8 A_u8Col) ;
+#endif /* HAL_HLCD_LCD_H_ */
